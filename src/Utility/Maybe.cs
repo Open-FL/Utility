cf5815b93cd1785ffeba55116c21f8843bcd8624
Tim Akermann <ti.akermann@gmail.com>
2020-10-05 09:31:57 +0200

    Added Maybe Implementation

---
+++ src/Utility/Maybe.cs
@@ -0,0 +1,29 @@
+﻿namespace Utility
+{
+    public class Maybe<T>
+    {
+
+        private T value;
+
+        public Maybe(T value)
+        {
+            this.value = value;
+        }
+
+        public T GetOrElse(T defaultValue)
+        {
+            return value == null ? defaultValue : value;
+        }
+
+    }
+
+    public static class MaybeExtensions
+    {
+
+        public static Maybe<T> ToMaybe<T>(this T value)
+        {
+            return new Maybe<T>(value);
+        }
+
+    }
+}
